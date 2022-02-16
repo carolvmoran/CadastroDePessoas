52a8df3d08f11a7b2bb95e1eebe756822cf963bc
Caroline <carolvandrade@gmail.com>
2022-02-16 18:37:02 -0400

    feature: iniciando projeto

---
+++ cadastro-de-pessoas/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div
+      className={`
+    flex h-screen justify-center items-center
+    bg-gradient-to-r from-purple-500 to-blue-600
+  `}
+    >
+      <span className="text-4xl">Texto</span>
+    </div>
+  );
+}
