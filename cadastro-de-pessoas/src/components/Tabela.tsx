f69f0ef97266d2ec91eb9281aa0afff599a5f8bc
Caroline <carolvandrade@gmail.com>
2022-02-16 19:21:02 -0400

    fix: iniciando estrutura da Tabela

---
+++ cadastro-de-pessoas/src/components/Tabela.tsx
@@ -0,0 +1,38 @@
+import Cliente from "../core/Cliente";
+interface TabelaProps {
+  clientes: Cliente[];
+}
+export default function Tabela(props: TabelaProps) {
+  function renderizarCabecalho() {
+    return (
+      <tr>
+        <th className="text-left p-4">Código</th>
+        <th className="text-left p-4">Nome</th>
+        <th className="text-left p-4">Idade</th>
+      </tr>
+    );
+  }
+
+  function renderizarDados() {
+    return props.clientes?.map((cliente, i) => {
+      return (
+        <tr
+          key={cliente.id}
+          className={`${i % 2 === 0 ? "bg-purple-200" : "bg-purple-100"}`}
+        >
+          <td className="text-left p-4">{cliente.id}</td>
+          <td className="text-left p-4">{cliente.nome}</td>
+          <td className="text-left p-4">{cliente.idade}</td>
+        </tr>
+      );
+    });
+  }
+  return (
+    <table className="w-full rounded-xl overflow-hidden">
+      <thead className="bg-gradient-to-r from-purple-500 to-purple-800 text-gray-100">
+        {renderizarCabecalho()}
+      </thead>
+      <tbody>{renderizarDados()}</tbody>
+    </table>
+  );
+}
--- cadastro-de-pessoas/src/pages/index.tsx
+++ cadastro-de-pessoas/src/pages/index.tsx
@@ -1,15 +1,27 @@
 import Head from "next/head";
 import Image from "next/image";
+import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
+import Cliente from "../core/Cliente";
 
 export default function Home() {
+  const clientes = [
+    new Cliente("Ana", 34, "1"),
+    new Cliente("Bia", 45, "2"),
+    new Cliente("Ana", 23, "3"),
+    new Cliente("Ana", 54, "4"),
+  ];
   return (
     <div
       className={`
     flex h-screen justify-center items-center
     bg-gradient-to-r from-purple-500 to-blue-600
+    text-white
   `}
     >
-      <span className="text-4xl">Texto</span>
+      <Layout titulo="Cadastro Simples">
+        <Tabela clientes={clientes}></Tabela>
+      </Layout>
     </div>
   );
 }
