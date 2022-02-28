ddf64256fb63ccc5ea70847c06e993edc5964360
Caroline <carolvandrade@gmail.com>
2022-02-28 16:10:25 -0400

    fix: ajuste para funcionar firebase

--- cadastro-de-pessoas/src/backend/config.ts
+++ cadastro-de-pessoas/src/backend/config.ts
@@ -1,5 +1,6 @@
+// v9 compat packages are API compatible with v8 code
 import firebase from 'firebase/compat/app';
-import "firebase/firestore";
+import 'firebase/compat/firestore';
 
 if(!firebase.apps.length) {
     firebase.initializeApp({
